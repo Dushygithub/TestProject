a6c1eb4ed05a4144054e3626289439a8431a5f26
bharath-achar <65662060+bharath-achar@users.noreply.github.com>
2023-11-04 19:46:17 +0530

    Commit

---
+++ LinearSearch/src/Kodnest/Linearsearch.java
@@ -0,0 +1,36 @@
+package Kodnest;
+
+import java.util.Scanner;
+
+public class Linearsearch {
+
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=scan.nextInt();
+		
+		int[] arr=new int[n];
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			arr[i]=scan.nextInt();
+			
+			
+		}
+		int ele=scan.nextInt();
+		
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			if(arr[i]==ele)
+			{
+				System.out.println("key is found in"+i);
+				return ;
+			}
+			
+			
+		}
+		
+		System.out.println("key is not found");
+	}
+
+}
